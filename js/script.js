a4a509430d6afaca40ee167f4f21be0bffc93839
fadwaMohamed <fadwa.ghazy@gmail.com>
2022-04-27 02:04:02 +0200

    header_services-p1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', function() {
+    document.getElementsByClassName('hamburger')[0].addEventListener('click', function() {
+        document.getElementsByClassName('links')[0].classList.toggle("hide");
+    })
+})
